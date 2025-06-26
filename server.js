9fe70c97478fd1eb116c95e4126a06122da9f7f6
Mike Jones <mikejones@Mikes-MacBook-Pro.local>
2025-06-26 22:01:21 +0100

    fix(server): Add OPTIONS handler for CORS preflight requests

--- server.js
+++ server.js
@@ -857,3 +857,13 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.status(204).end();
+  }
+  next();
+});
